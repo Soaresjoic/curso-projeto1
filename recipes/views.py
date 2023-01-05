e87b89f7b53c047b83e04cd9e89a6f5c9a598309
Joice Soares <soaresjoic@gmail.com>
2023-01-05 16:35:49 -0300

    criada a pasta templates e adicionado o html

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('Home')
+    return render(request, 'recipes/home.html')
+# a pasta recipes foi criada para evitar confusão de diretório de pastas
 
 
 def sobre(request):
