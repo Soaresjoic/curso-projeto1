4379aad5ceac0aec4c1f8827f775263a6cbe1c8a
Joice Soares <soaresjoic@gmail.com>
2023-01-06 18:49:22 -0300

    html e css quase finalizado e separados por pastas

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html')
 # a pasta 'recipes' foi criada para evitar confusão de diretório de pastas
-
