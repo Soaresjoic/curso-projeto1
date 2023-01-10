5f7b563cc191b35752d1278728278489eaf48d68
Joice Soares <soaresjoic@gmail.com>
2023-01-10 15:01:05 -0300

    inserindo dados nas views home e recipe

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
+    })
+
+# a função importada make_recipes vai criar dados aleatórios para nosso
+# formulário.Nesse caso, irá gerar 10 receitas aleatórias para a home
+
+
 # a pasta 'recipes' foi criada para evitar confusão de diretório de pastas
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
