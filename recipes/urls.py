78a8c6764fd04f8cc10c6c824d0c7237dc302292
Joice Soares <soaresjoic@gmail.com>
2023-01-10 15:44:17 -0300

    namespace com app_name

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views  # ou from recipe import views
 
+# recipes:recipe
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),  # home
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name="home"),  # home
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
